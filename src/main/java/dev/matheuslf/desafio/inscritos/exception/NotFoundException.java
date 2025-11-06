f48fa12d3310d1812f0668740a356771f5c38b49
Leonardo Lima <leo2342005@gmail.com>
2025-11-06 16:48:57 -0300

    feat: adicionando tratamento de NotFound.

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package dev.matheuslf.desafio.inscritos.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/dev/matheuslf/desafio/inscritos/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.matheuslf.desafio.inscritos.exception.handler;
 
 import dev.matheuslf.desafio.inscritos.dto.error.ResponseError;
+import dev.matheuslf.desafio.inscritos.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -15,12 +16,20 @@ import java.util.List;
 @Slf4j
 public class GlobalExceptionHandler{
 
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseError handleNotFoundException(NotFoundException e) {
+        log.error("Resource not found: {}", e.getMessage());
+        return new ResponseError(HttpStatus.NOT_FOUND.value(), e.getMessage(), List.of());
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ResponseError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.error("Erro de validação: {}", e.getMessage());
+        log.error("Validation error: {}", e.getMessage());
         List<FieldError> fieldErrors = e.getFieldErrors();
         List<dev.matheuslf.desafio.inscritos.dto.error.FieldError> errorsList = fieldErrors.stream().map(fe -> new dev.matheuslf.desafio.inscritos.dto.error.FieldError(fe.getField(), fe.getDefaultMessage())).toList();
         return new ResponseError(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Validation error.", errorsList);
     }
+
 }
