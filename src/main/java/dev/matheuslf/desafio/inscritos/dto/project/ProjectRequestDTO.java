d6d7a25fc68b7a2a4f99a733f53d7906455655ff
Leonardo Lima <leo2342005@gmail.com>
2025-11-06 16:49:27 -0300

    feat: adicionando mensagens de erro de validação nos requests do projeto.

--- src/main/java/dev/matheuslf/desafio/inscritos/dto/project/ProjectRequestDTO.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/dto/project/ProjectRequestDTO.java
@@ -6,13 +6,13 @@ import java.time.LocalDate;
 
 public record ProjectRequestDTO(
         @NotBlank
-        @Size(min = 3, max = 100)
+        @Size(min = 3, max = 100, message = "Name cannot be shorter than 3 characters or longer than 100 characters.")
         String name,
-        @Size(max = 255)
+        @Size(max = 255, message = "Description cannot be longer than 255 characters.")
         String description,
-        @FutureOrPresent
+        @FutureOrPresent(message = "Start date must be in the present or future")
         LocalDate startDate,
-        @Future
+        @Future(message = "End date must be in the future")
         LocalDate endDate
 ) {
 }
