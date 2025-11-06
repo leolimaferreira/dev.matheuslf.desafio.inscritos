3a064c67b01c8e613571b34a9e413f3688b20be0
Leonardo Lima <leo2342005@gmail.com>
2025-11-06 14:40:37 -0300

    feat: adicionando dependência para variaveis de ambiente no projeto.

--- src/main/java/dev/matheuslf/desafio/inscritos/InscritosApplication.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/InscritosApplication.java
@@ -1,13 +1,23 @@
 package dev.matheuslf.desafio.inscritos;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InscritosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InscritosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("env")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(InscritosApplication.class, args);
+    }
 
 }
