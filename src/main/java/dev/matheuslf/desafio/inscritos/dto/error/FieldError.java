e4829ef1486e07885ff86d1b729896f645b22e3a
Leonardo Lima <leo2342005@gmail.com>
2025-11-20 00:13:48 -0300

    docs: adicionando documentacao nos dtos de erro.

--- src/main/java/dev/matheuslf/desafio/inscritos/dto/error/FieldError.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/dto/error/FieldError.java
@@ -1,7 +1,12 @@
 package dev.matheuslf.desafio.inscritos.dto.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "Erro de Campo", description = "Representa um erro de validação em um campo específico")
 public record FieldError(
+        @Schema(name = "campo", description = "Nome do campo que contém o erro", example = "email")
         String field,
+        @Schema(name = "erro", description = "Mensagem de erro do campo", example = "O email não pode estar vazio")
         String error
 ) {
 }
--- src/main/java/dev/matheuslf/desafio/inscritos/dto/error/ResponseError.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/dto/error/ResponseError.java
@@ -1,25 +1,22 @@
 package dev.matheuslf.desafio.inscritos.dto.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+@Schema(name = "Resposta de Erro", description = "Representa uma resposta de erro")
 public record ResponseError(
+        @Schema(name = "status", description = "Status do erro", example = "404")
         int status,
+        @Schema(name = "mensagem", description = "Mensagem de erro", example = "Usuário nao encontrado")
         String message,
+        @Schema(name = "erros", description = "Nomes dos campos que contém os erros")
         List<FieldError> errors
 ) {
 
-    public static ResponseError defaultResponse(String message) {
-        return new ResponseError(HttpStatus.BAD_REQUEST.value(), message, List.of());
-    }
-
     public static ResponseError conflict(String mensagem) {
         return new ResponseError(HttpStatus.CONFLICT.value(), mensagem, List.of());
-    }
-
-    public static ResponseError unprocessbleEntity(String mensagem) {
-        return new ResponseError(HttpStatus.UNPROCESSABLE_ENTITY.value(), mensagem, List.of());
     }
 
     public static ResponseError of(HttpStatus status, String message, List<FieldError> errors) {
